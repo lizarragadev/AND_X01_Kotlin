79e3902778dfbfa4617b4fe0f1eef4b1d776699a
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-08-24 22:13:30 -0400

    nulos and final

---
+++ 00_KotlinSamples/k_16_exam/ejercicio4/Enfermera.kt
@@ -0,0 +1,12 @@
+
+class Enfermera(nom: String, ape: String, ed: Int, var codInterno: String) : Personal(nom, ape, ed) {
+    init {
+        if (codInterno == "")
+            codInterno = "DEF-00001"
+    }
+
+    override fun imprimirPersonal() {
+        super.imprimirPersonal()
+        println("Codigo interino: $codInterno")
+    }
+}
