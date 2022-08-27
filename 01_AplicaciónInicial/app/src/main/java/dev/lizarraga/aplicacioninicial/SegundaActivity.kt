9df205703c76ac764a9e96e45c94b65955d31e1d
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-08-26 23:52:20 -0400

    Add initial application

---
+++ 01_AplicaciónInicial/app/src/main/java/dev/lizarraga/aplicacioninicial/SegundaActivity.kt
@@ -0,0 +1,11 @@
+package dev.lizarraga.aplicacioninicial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda)
+    }
+}
