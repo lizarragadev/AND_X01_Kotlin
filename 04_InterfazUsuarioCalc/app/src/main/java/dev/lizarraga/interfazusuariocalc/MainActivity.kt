a6406a1ddb86ca6da06380ed0c437dcd3b2f3ef3
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-08-29 22:12:28 -0400

    add calc demo

---
+++ 04_InterfazUsuarioCalc/app/src/main/java/dev/lizarraga/interfazusuariocalc/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.lizarraga.interfazusuariocalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+    }
+}
