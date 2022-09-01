45348462a93b7ae6a641185563cca3954dfd4b28
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-08-31 22:20:00 -0400

    sample personalizacion

---
+++ 05_Personalizacion_1/app/src/main/java/dev/lizarraga/personalizacion1/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.lizarraga.personalizacion1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+    }
+
+}
