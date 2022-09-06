60677a8883cb1ce0e6ebd45f2bdc76a556d6c976
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-09-05 22:18:45 -0400

    Final code pedidos

--- 07_Pedidos/app/src/main/java/dev/lizarraga/pedidos/MainActivity.kt
+++ 07_Pedidos/app/src/main/java/dev/lizarraga/pedidos/MainActivity.kt
@@ -2,10 +2,41 @@ package dev.lizarraga.pedidos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import dev.lizarraga.pedidos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var cantidad = 1
+    val precio = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvCantidad.text = cantidad.toString()
+    }
+    fun menos(view: View) {
+        if(cantidad > 1)
+            cantidad--
+
+        binding.tvCantidad.text = cantidad.toString()
+    }
+    fun mas(view: View) {
+        cantidad++
+        binding.tvCantidad.text = cantidad.toString()
+    }
+    fun calcular(view: View) {
+        if (binding.etNombre.text.toString() != "") {
+            binding.tvResumenCantidad.text = cantidad.toString()
+            binding.tvResumenPrecioUnitario.text = "Bs. $precio"
+            binding.tvResumenCostoTotal.text = "Bs. ${cantidad * precio}"
+            binding.tvResumenNombre.text = binding.etNombre.text.toString()
+        } else {
+            Toast.makeText(this,
+                "Escriba un nombre",
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
